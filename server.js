c862836c0788856209a543fcbc48a706cc8479d1
fedor <nappingwhale@gmail.com>
2015-10-08 18:42:10 +0000

    HW 5 latest change

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
-app.get('/netid', function(request, response){
-    response.send('Hello raccoon');
+app.get('/nickname', function(request, response){
+    response.send('stormy-quail');
 });
 
 app.listen(process.env.PORT || 4000);
