7db955d46aa1e38c8a2ce05ded92dda049f4cbe5
fedor <nappingwhale@gmail.com>
2015-10-08 18:17:28 +0000

    HW5 Add

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/netid', function(request, response){
+    response.send('Hello raccoon');
+});
+
 app.listen(process.env.PORT || 4000);
